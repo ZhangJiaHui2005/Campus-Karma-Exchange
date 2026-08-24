1d4f05bf914ce6c03d81656e9535286e8e5cfecf
ZhangJiaHui2005 <huygiatruong2005@gmail.com>
2026-08-24 21:40:00 +0700

    thêm tailwindcss và flowbite react

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,11 @@
+import { Button } from "flowbite-react"
+
 function Home() {
   return (
     <div className="page">
       <h1>Home Page</h1>
       <p>Welcome to Campus Karma Exchange</p>
+      <Button>Click Me</Button>
     </div>
   )
 }
