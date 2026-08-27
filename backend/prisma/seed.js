bb636ffed672bfc7fb126f3dc09e10ab1e68e23d
ZhangJiaHui2005 <huygiatruong2005@gmail.com>
2026-08-27 08:08:17 +0700

    sua lai ten bang

--- backend/prisma/seed.js
+++ backend/prisma/seed.js
@@ -15,25 +15,17 @@ async function main() {
   //     { level_id: 3, level_name: 'Đại sứ Xanh', min_karma: 1001, max_karma: 999999, borrow_limit: 10, deposit_discount_pct: 50 },
   //   ],
   // });
-  // await prisma.category.createMany({
-  //   skipDuplicates: true,
-  //   data: [
-  //     { name: 'Sach va tai lieu', icon: 'book-open' },
-  //     { name: 'Do dien tu', icon: 'laptop' },
-  //     { name: 'Quan ao', icon: 'shirt' },
-  //     { name: 'Do gia dung', icon: 'package' },
-  //     { name: 'The thao', icon: 'dumbbell' },
-  //     { name: 'Khac', icon: 'circle-ellipsis' },
-  //   ],
-  // });
-
-  await prisma.admin.createMany({
+  await prisma.category.createMany({
     skipDuplicates: true,
     data: [
-      { email: 'huunhan882005@gmail.com', password_hash: await bcrypt.hash("12345678", await bcrypt.genSalt(10)), full_name: 'Tran Huu Nhan' },
-    ]
+      { name: 'Sach va tai lieu', icon: 'book-open' },
+      { name: 'Do dien tu', icon: 'laptop' },
+      { name: 'Quan ao', icon: 'shirt' },
+      { name: 'Do gia dung', icon: 'package' },
+      { name: 'The thao', icon: 'dumbbell' },
+      { name: 'Khac', icon: 'circle-ellipsis' },
+    ],
   });
-  console.log('Seed dữ liệu ADMIN thành công!');
 }
 
 main().catch(console.error).finally(() => prisma.$disconnect());
